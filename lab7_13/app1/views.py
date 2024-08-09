6e6bd3b9e15b229144b51d4309c4002f329aa451
Alan George JImcy <117376928+1rn21cs018alan@users.noreply.github.com>
2024-08-09 17:22:06 +0530

    Add files via upload

--- lab7_13/app1/views.py
+++ lab7_13/app1/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from app1.forms import *
 from app1.models import *
 from django.views.generic import DetailView,ListView
+import csv
+from reportlab.pdfgen import canvas
 def is_ajax(req):
     return req.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 def add_project(req):
@@ -11,38 +13,51 @@ def add_project(req):
         if form.is_valid():
             form.save()
         return render(req, 'add_project.html',locals())
-    else:
-        form=ProjectForm()
-        return render(req,'add_project.html',locals())
+    form=ProjectForm()
+    return render(req,'add_project.html',locals())
 def add_student(req):
-    print("call")
-    if req.method=='POST':
+    if is_ajax(req):
         form=StudentForm(req.POST)
         if form.is_valid():
             form.save()
-        print("ye")
-        if is_ajax(req):
-            return JsonResponse({'success':True})
-        else:
-            return render(req, 'add_student.html',locals()) 
-    else:
-        form=StudentForm()
-        return render(req,'add_student.html',locals())
+        return JsonResponse({'success':form.is_valid()})
+    form=StudentForm()
+    return render(req,'add_student.html',locals())
 def search(req):
     if is_ajax(req):
-        # print("read as ajax",req.GET.get('usn'))
         student=Student.objects.get(usn=req.GET.get('usn'))
-        # print(student.courses.all())
-        courses=""
-        for i in student.courses.all():
-            courses+=i.name+","
+        courses=",".join(str(j) for j in student.courses.all())
         data={'name':student.name,'usn':student.usn,'courses':courses}
-        # print(data)
         return JsonResponse(data)
-    else:
-        return render(req,"search.html")
-    
-    
+    return render(req,"search.html")
+def get_csv(req):
+    res=HttpResponse(content_type="text/csv")
+    res['Content-Disposition'] = 'attachment; filename="student.csv"'
+    writer=csv.writer(res)
+    writer.writerow(['name', 'usn', 'courses'])
+    for i in Student.objects.all():
+        courses=",".join(str(j) for j in i.courses.all())
+        writer.writerow([i.name,i.usn,courses])
+    return res
+def get_pdf(req):
+    res=HttpResponse(content_type="application/pdf")
+    res['Content-Disposition'] = 'attachment; filename="student.pdf"'
+    p = canvas.Canvas(res, pagesize=(600,800))
+    y=750
+    def entry(name,usn,course):
+        d=p.drawString
+        d(50,y,name)
+        d(200,y,usn)
+        d(300,y,course)
+    entry('name','usn','courses')
+    for i in Student.objects.all():
+        courses=",".join(str(j) for j in i.courses.all())
+        y-=15
+        entry(i.name,i.usn,courses)
+    p.showPage()
+    p.save()
+    return res
+
 class StudentListView(ListView):
     model=Student
     template_name="students.html"
